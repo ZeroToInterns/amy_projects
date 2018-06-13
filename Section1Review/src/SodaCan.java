19fbdb1c2cab81567a2f249795465d23bf84bd59
Amy Phan <amyyyyphan99@gmail.com>
2018-06-13 10:10:23 -0700

    Finished Problem 2

--- Section1Review/src/SodaCan.java
+++ Section1Review/src/SodaCan.java
@@ -1,4 +1,66 @@
 
-public class SodaCan {
+public class SodaCan implements Comparable{
 
+	public static final double DEFAULT_RADIUS = 1.2109;
+	public static final double DEFAULT_HEIGHT = 4.704;
+	public static final double FLUID_OUNCES_PER_CUBIC_INCHES = 0.554112554;
+	    
+    private double radius;
+	private double height;
+	private double content;
+	
+	//default constructor
+	public SodaCan() {
+		radius = DEFAULT_RADIUS;
+		height = DEFAULT_HEIGHT;
+		content = Math.PI * radius * radius * height * FLUID_OUNCES_PER_CUBIC_INCHES;
+	}
+	
+	//constructor with given radius and height
+	public SodaCan(double theRadius, double theHeight) {
+		if (theRadius < 0 || theHeight < 0) {
+			this.radius = DEFAULT_RADIUS;
+			this.height = DEFAULT_HEIGHT;
+		}
+		else {
+			this.radius = theRadius;
+			this.height = theHeight;
+		}
+		content = Math.PI * radius * radius * height * FLUID_OUNCES_PER_CUBIC_INCHES;
+	}
+	
+	//gets the radius of the SodaCan
+	public double getRadius() {
+		return radius;
+	}
+	
+	//gets the height of the SodaCan
+	public double getHeight() {
+		return height;
+	}
+	
+	//simulates drinking from SodaCan by subtracting fluidOunces from the contents
+	public void drink(double fluidOunces) {
+		content -= fluidOunces;
+	}
+	
+	//gets the fl. oz. of soda remaining in the SodaCan
+	public double getContents() {
+		return content;
+	}
+	
+	@Override
+	//compares the current content of the SodaCan to another SodaCan
+	public int compareTo(Object o) {
+		SodaCan otherCan = (SodaCan) o;
+		if (content > otherCan.content) {
+			return 1;
+		}
+		else if (content < otherCan.content) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
 }
