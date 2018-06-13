5a89b42f8275292e5b1eec5bd02118531e55931b
Amy Phan <amyyyyphan99@gmail.com>
2018-06-13 10:13:06 -0700

    Finished Problem 1

--- Section1Review/src/ElectricCar.java
+++ Section1Review/src/ElectricCar.java
@@ -1,4 +1,42 @@
 
 public class ElectricCar {
 
+	private int maxRange;
+	private int currentRange;
+	private String make;
+	
+	//constructor
+	public ElectricCar(String make, int maxRange) {
+		this.make = make;
+		this.maxRange = maxRange;
+		currentRange = 0;
+	}
+	
+	//charges the ElectricCar to its maximum range
+	public void charge() {
+		currentRange = maxRange;
+	}
+	
+	//drives the car the given distance
+	public void drive(int miles) {
+		if (miles > currentRange) {
+			System.out.println("You can't drive that far");
+		}
+		currentRange -= miles;
+	}
+	
+	//gets the make of the ElectricCar
+	public String getMake() {
+		return make;
+	}
+	
+	//gets the number of miles the car can go
+	public int getCurrentRange() {
+		return currentRange;
+	}
+	
+	//gets the maximum number of miles the car can go when fully charged
+	public int getMaxRange() {
+		return maxRange;
+	}
 }
