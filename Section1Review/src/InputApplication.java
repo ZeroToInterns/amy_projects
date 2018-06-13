e5ae1274f60cf7a8720f5737b6e0d1ca889d4747
Amy Phan <amyyyyphan99@gmail.com>
2018-06-13 10:23:48 -0700

    Finished Problem 3

--- Section1Review/src/InputApplication.java
+++ Section1Review/src/InputApplication.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 public class InputApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		//asks user for their favorite dessert and prints the last letter of it
+		System.out.print("Enter a your favorite dessert: ");
+		String favoriteDessert = scan.nextLine();
+		System.out.println(favoriteDessert.substring(favoriteDessert.length() - 1,  favoriteDessert.length()));
+		
+		//asks user for a double
+		System.out.print("Enter a double: ");
+		double aDouble = scan.nextDouble();
+		
+		//asks user for an integer
+		System.out.print("Enter an integer: ");
+		int integer = scan.nextInt();
+		
+		//prints the product of the double and integer 
+		double product = aDouble * integer;
+		System.out.println("Product: " + product);
+		
+		//prints the integer portion of the product
+		int integerPortion = (int) product;
+		System.out.println("Integer Portion: " + integerPortion);
 	}
 
 }
