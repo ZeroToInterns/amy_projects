66fcff3b92d42922abf576d22f415a84062dec8c
Amy Phan <amyyyyphan99@gmail.com>
2018-06-14 11:31:07 -0700

    Finished Problem 7

--- Section1Review/src/LoanApplicant.java
+++ Section1Review/src/LoanApplicant.java
@@ -33,7 +33,10 @@ public class LoanApplicant {
 	
 	//sets a number of years on the job
 	public void setYearsOnJob(int years) {
-		if (years >= 0) {
+		if (years < 0) {
+			System.out.println("Years on job cannot be a negative");
+		}
+		else {
 			this.yearsOnJob = years;
 		}
 	}
@@ -45,6 +48,11 @@ public class LoanApplicant {
 	
 	//sets a new annual salary
 	public void setAnnualSalary(double salary) {
-		this.annualSalary = salary;
+		if (salary < 0) {
+			System.out.println("Annual salary cannot be a negative");
+		}
+		else {
+			this.annualSalary = salary;
+		}
 	}
 }
