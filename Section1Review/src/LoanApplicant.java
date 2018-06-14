074fcbbfdf983f5b47a79750473d117fce0224d6
Amy Phan <amyyyyphan99@gmail.com>
2018-06-14 10:18:34 -0700

    Finished Problem 7

---
+++ Section1Review/src/LoanApplicant.java
@@ -0,0 +1,50 @@
+
+public class LoanApplicant {
+
+	private double annualSalary;
+	private int yearsOnJob;
+	
+	//constructor
+	public LoanApplicant(double annualSalary, int yearsOnJob) {
+		this.annualSalary = annualSalary;
+		this.yearsOnJob = yearsOnJob;
+	}
+	
+	//returns true if the applicant qualifies otherwise it returns false
+	public boolean qualifies() {
+		if (annualSalary > 30000) {
+			if (annualSalary > 100000) {
+				return true;
+			}
+			else if (annualSalary <= 100000 && annualSalary > 50000 && yearsOnJob >= 2) {
+				return true;
+			}
+			else if (annualSalary <= 50000 && yearsOnJob >= 5) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;	
+		}
+	}
+	
+	//sets a number of years on the job
+	public void setYearsOnJob(int years) {
+		if (years >= 0) {
+			this.yearsOnJob = years;
+		}
+	}
+	
+	//gets the number of years on the job
+	public int getYearsOnJob() {
+		return yearsOnJob;
+	}
+	
+	//sets a new annual salary
+	public void setAnnualSalary(double salary) {
+		this.annualSalary = salary;
+	}
+}
